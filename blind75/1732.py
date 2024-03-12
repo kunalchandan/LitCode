1f1fc5e0c11fbb4b91e018f207fec5113d0a4ff8
Kunal Chandan <kunal_chandan@outlook.com>
2024-03-12 01:15:28 -0700

    add simple solution

--- blind75/1732.py
+++ blind75/1732.py
@@ -2,5 +2,11 @@ from typing import List
 
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        # max_alt = 0
+        # alt = 0
+        # for i, v in enumerate(gain):
+        #     alt = alt + v
+        #     max_alt = max(max_alt, alt)
+        # return max_alt
         return max(max(sum(gain[:i+1]) for i in range(len(gain))), 0)
 
